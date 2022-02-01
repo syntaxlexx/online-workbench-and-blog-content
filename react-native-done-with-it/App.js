e313705361a80544b192c5b454a59e70f791e12f
Lexx YungCarter <spencermainaa@gmail.com>
2022-02-02 01:39:59 +0300

    feat: network State detection

--- react-native-done-with-it/App.js
+++ react-native-done-with-it/App.js
@@ -2,8 +2,16 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
+import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
 
 export default function App() {
+  const netInfo = useNetInfo();
+  if (!netInfo.isInternetReachable) {
+    console.log("Network Unreachable!");
+  } else {
+    console.log("Network is Fine!");
+  }
+
   return (
     <NavigationContainer theme={navigationTheme}>
       <AppNavigator />
